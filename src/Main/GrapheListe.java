2bd7d4b9ae51f9d0e0f5a770b71a0ede6ed9bde7
Pegland64 <haution2@etu.univ-lorraine.fr>
2024-05-28 17:04:32 +0200

    nul

---
+++ src/Main/GrapheListe.java
@@ -0,0 +1,15 @@
+package Main;
+
+import java.util.*;
+
+public class GrapheListe {
+    ArrayList<String> noeuds;
+    ArrayList<Arcs> adjacence;
+
+    public GrapheListe() {
+        this.noeuds = new ArrayList<String>();
+        this.adjacence = new ArrayList<Arcs>();
+    }
+
+    
+}
