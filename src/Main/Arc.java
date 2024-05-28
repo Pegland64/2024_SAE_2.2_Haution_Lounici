b0adbc153d4c002b21057e1353fb86e0150ecfc8
Pegland64 <haution2@etu.univ-lorraine.fr>
2024-05-28 16:38:13 +0200

    Arc.java

--- src/Main/Arc.java
+++ src/Main/Arc.java
@@ -1,5 +1,11 @@
 package Main;
 
 public class Arc {
-    
+    private String dest;
+    private double cout;
+
+    public Arc(String dest, double cout) {
+        this.dest = dest;
+        this.cout = cout;
+    }
 }
